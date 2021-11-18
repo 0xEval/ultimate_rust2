e2b8dd441336e6a33d6b0672033a47bc8570b2d4
0xEval <julien.couvy@gmail.com>
2021-11-18 10:29:27 +0800

    Exercise 5 added

--- exercise/errors/src/main.rs
+++ exercise/errors/src/main.rs
@@ -1,6 +1,7 @@
 // START IN lib.rs!
 
 use aquarium::Dolphin;
+use anyhow::Result;
 
 // (You already did #1 in lib.rs, right?)
 //
@@ -9,22 +10,57 @@ use aquarium::Dolphin;
 // - Have the play_time function return a `Result<Vec<String>>`. The vector of Strings will
 //   represent successful outcomes of various dolphin tricks.
 
-// fn play_time(dolphin: &Dolphin) -> ... {
-//     let mut responses = vec![];
-//     // 2b. Call the .say_your_name() method on `dolphin`, use `?` to unwrap the value, and push
-//     // the value onto the `responses` vector.
-//     //
-//     // let response = ...    // this can be done with an intermediate variable...
-//     // responses.push( ... ) // ...or all on one line. Either way is fine!
-//     //
-//     // 2c. Do the same thing as #2b for the .flip() method
-//     //
-//     // 2d. Do the same thing as #2b for the .shake_hands() method
-//
-//     Ok(responses)
+fn play_time(dolphin: &Dolphin) -> Result<Vec<String>> {
+ let mut responses = vec![];
+ // 2b. Call the .say_your_name() method on `dolphin`, use `?` to unwrap the value, and push
+ // the value onto the `responses` vector.
+ //
+ // let response = ...    // this can be done with an intermediate variable...
+ responses.push(dolphin.say_your_name()?); // ...or all on one line. Either way is fine!
+ // 2c. Do the same thing as #2b for the .flip() method
+ responses.push(dolphin.flip()?);
+ // 2d. Do the same thing as #2b for the .shake_hands() method
+ responses.push(dolphin.shake_hands()?);
+
+ Ok(responses)
+}
+
+// fn main() -> Result<(), anyhow::Error> {
+//     let dolphins = vec![
+//         // Dolphin {
+//         //     name: "Augustinius".into(),
+//         //     age: 7,
+//         //     hungry: false,
+//         // },
+//         // Dolphin {
+//         //     name: "Bitty".into(),
+//         //     age: 2,
+//         //     hungry: true,
+//         // },
+//         // Dolphin {
+//         //     name: "Carson".into(),
+//         //     age: 5,
+//         //     hungry: true,
+//         // },
+//         Dolphin {
+//             name: "Devin".into(),
+//             age: 6,
+//             hungry: false,
+//         },
+//     ];
+// 
+//     for dolphin in &dolphins {
+//         for _resp in play_time(dolphin)? {
+//             println!("{} did a FABULOUS PERFORMANCE!", dolphin.name);
+//             println!("  {}", _resp);
+//         }
+//     }
+// 
+//     Ok(())
 // }
 
 fn main() {
+
     let dolphins = vec![
         Dolphin {
             name: "Augustinius".into(),
