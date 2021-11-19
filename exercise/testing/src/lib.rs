e478564a4f80f557b1bf60f68460793617c67d8e
0xEval <julien.couvy@gmail.com>
2021-11-19 11:42:29 +0800

    Exercise Testing Done

--- exercise/testing/src/lib.rs
+++ exercise/testing/src/lib.rs
@@ -1,35 +1,45 @@
+#[inline]
 pub fn sploosh(x: i32, y: i32, z: i32) -> i32 {
+    if x < 0 { 99; }
+
     match (x, y, z) {
-        (x, _, _) if x < 0 => 99,
         (1, 2, 3) => 4,
         (5, 6, 7) => 3,
-        (x, y, z) => x + y - z,
-    }
+        _ => x + y - z
+     }
 }
 
 pub fn splish(a: i32, b: i32) -> i32 {
     -a + 3 * b
 }
 
-// 1. Use the `cfg` attribute to mark the `test` module below as a test module
-
+#[cfg(test)]
 mod test {
     // 2. Bring all the library items into scope with a `use` statement
     // Hint: It's okay to use `*` here.
+    use super::{splish, sploosh};
 
-    // 3. Write a test function that verifies the following condition using the `assert_eq!` or
-    // `assert_ne!` macros
-    // - sploosh(1, 2, 3) returns 4
-    // - sploosh(5, 6, 7) does not return 4
-    // - If you pass sploosh a negative number for the first argument, 99 is returned
-    //
     // `cargo test` should run your tests and pass
-    // Hint: Don't forget the `#[test]` attribute for your test function!
 
+    #[test]
+    fn sploosh_expected_vals() {
+        // - sploosh(1, 2, 3) returns 4
+        assert_eq!(sploosh(1, 2, 3), 4);
+        // - If you pass sploosh a negative number for the first argument, 99 is returned
+        assert_eq!(sploosh(-1, 1, 1), 99);
+        // - sploosh(5, 6, 7) does not return 4
+        assert_ne!(sploosh(5, 6, 7), 4);
+    }
     // 4. Write a test function that verifies the following conditions using the `assert!` macro
-    // - splish(100, 10) is negative
-    // - splish(40, 20) is positive
-    // - splish(9, 3) is 0
+    #[test]
+    fn splish_expected_vals() {
+        // - splish(100, 10) is negative
+        assert!(splish(100, 10) < 0);
+        // - splish(40, 20) is positive
+        assert!(splish(40, 20) > 0);
+        // - splish(9, 3) is 0
+        assert!(splish(9, 3) == 0);
+    }
 }
 
 // 4. Create a `tests/` directory and an integration test file `tests/more_tests.rs`
